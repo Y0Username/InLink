18925a5d471a6c16e0a33e9695a31b35894342ab
Manik Narang <narang_manik@rocketmail.com>
2017-01-14 02:31:17 -0800

    Facebook: Add their login API

--- app/src/main/java/com/example/android/interact/MainActivity.java
+++ app/src/main/java/com/example/android/interact/MainActivity.java
@@ -1,27 +1,99 @@
 package com.example.android.interact;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
+import com.facebook.FacebookSdk;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView qrCode;
     private String qrString;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private AccessToken accessToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Track App Installs and App Opens
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
+
+        AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("FACEBOOK MANAGER", "SUCCESS");
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("FACEBOOK MANAGER", "CANCEL");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("FACEBOOK MANAGER", "ERROR");
+            }
+        });
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                accessToken = currentAccessToken;
+            }
+        };
+
         qrCode = (ImageView) findViewById(R.id.qr_code);
         new QRTask().execute("MyQRCode");
+    }
 
+
+    public void facebookLoginHandle(View v) {
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+        //LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("user_status"));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     class QRTask extends AsyncTask<String, String, Bitmap> {
