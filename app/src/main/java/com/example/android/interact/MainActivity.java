cfa0883b6552cf80e984be41e3a0b787d0d21e9c
Manik Narang <narang_manik@rocketmail.com>
2017-01-14 00:26:03 -0800

    Initial Commit: ZXing QR Implementation

---
+++ app/src/main/java/com/example/android/interact/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.android.interact;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView qrCode;
+    private String qrString;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        qrCode = (ImageView) findViewById(R.id.qr_code);
+        new QRTask().execute("MyQRCode");
+
+    }
+
+    class QRTask extends AsyncTask<String, String, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            Bitmap bitmap = null;
+            try{
+                bitmap = encodeAsBitmap(params[0]);
+            } catch (WriterException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if(bitmap !=null)
+                qrCode.setImageBitmap(bitmap);
+        }
+    }
+
+    private Bitmap encodeAsBitmap(String qr) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(qr, BarcodeFormat.QR_CODE, 500, 500, null);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w*h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? getResources().getColor(R.color.black):getResources().getColor(R.color.white);
+
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels,0,500,0,0,w,h);
+        return bitmap;
+    }
+}
