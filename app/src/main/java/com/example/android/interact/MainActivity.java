60bbd6fd250913c66fac6352bd8071d4c3943307
Anmol Ahuja <me@anmolahuja.com>
2017-01-14 01:20:14 -0800

    Configure stuff for facebook login.

--- app/src/main/java/com/example/android/interact/MainActivity.java
+++ app/src/main/java/com/example/android/interact/MainActivity.java
@@ -11,6 +11,8 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
+import com.facebook.FacebookSdk;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView qrCode;
