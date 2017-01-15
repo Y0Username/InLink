59b8047d52e94b318a126facd0063ecec173e5aa
Manik Narang <narang_manik@rocketmail.com>
2017-01-15 00:00:16 -0800

    dgfjewgfewjf

---
+++ app/src/main/java/com/example/android/interact/EditTextActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.interact;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by kingpushpakraj on 14-01-2017.
+ */
+
+public class EditTextActivity extends AppCompatActivity {
+    private EditText editText;
+    private EditText nameText;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_text);
+        editText = (EditText) findViewById(R.id.prg_text);
+        nameText = (EditText) findViewById(R.id.grp_text);
+    }
+
+    public void theButton(View v) {
+        String num = editText.getText().toString();
+        String phon = nameText.getText().toString();
+        SharedPreferences sh = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor shty = sh.edit();
+        shty.putString("phone", num);
+        shty.putString("name", phon);
+        shty.apply();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
